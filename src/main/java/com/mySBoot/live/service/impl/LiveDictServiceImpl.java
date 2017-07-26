01bae6b7ce6cb67bf6fb0e525b86412d2eb9db8c
kevinjames <275648393@qq.com>
2017-07-26 10:18:51 +0800

    误删的类恢复

--- src/main/java/com/mySBoot/live/service/impl/LiveDictServiceImpl.java
+++ src/main/java/com/mySBoot/live/service/impl/LiveDictServiceImpl.java
@@ -1,25 +1,33 @@
 package com.mySBoot.live.service.impl;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import com.mySBoot.live.pojo.LiveDict;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
 import com.google.common.collect.Lists;
+import com.mySBoot.live.controller.LiveController;
 import com.mySBoot.live.dao.LiveDictMapper;
+import com.mySBoot.live.pojo.LiveDict;
 import com.mySBoot.live.service.LiveDictService;
 
 /**
-* created on 2017-7-24 14:43:17
-*/
+ * 
+ * @author chenzhangwei
+ * @time 2017年7月26日上午10:18:38
+ */
 @Service
 public class LiveDictServiceImpl implements LiveDictService {
 
+	
     @Autowired
     private LiveDictMapper liveDictMapper;
+    
+    private static Logger logger = LoggerFactory.getLogger(LiveDictServiceImpl.class);
 
 	@Override
 	public List<LiveDict> getLiveDict(String value) {
@@ -27,6 +35,13 @@ public class LiveDictServiceImpl implements LiveDictService {
 		Map<Object, Object> map = new HashMap<>();
 		map.put("value", value);
 		list = liveDictMapper.selectDictTree(map);
+		logger.info("hhh");
+		logger = LoggerFactory.getLogger(LiveController.class);
+		try {
+			Thread.sleep(1000*5);
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
 		return list;
 	}
 
